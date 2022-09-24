375e83a2f4ea5774ebe06a84ca3da51ba684cd10
Anna Molodtsova <renasafetysea@mail.ru>
2022-09-24 15:08:27 +0300

    1 task

---
+++ src/Human.java
@@ -0,0 +1,13 @@
+public class Human {
+     int yearOfBirds;
+     String name;
+    String town;
+
+    void say(){
+        System.out.println("Привет! Меня зовут " + name);
+        System.out.println("Я из города " + town);
+        System.out.println("Я родился в " + name + "году. Будем знакомы");
+
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Human maksim = new Human();
+        maksim.name = "Максим";
+        maksim.town = "Минск";
+        maksim.yearOfBirds = 35;
+
+        maksim.say();
+        System.out.println();
+
+        Human anya = new Human();
+        anya.name = "Аня";
+        anya.town = "Москва";
+        anya.yearOfBirds = 29;
+
+        anya.say();
+        System.out.println();
+
+
+        Human katya = new Human();
+        katya.name = "Катя";
+        katya.town = "Калининград";
+        katya.yearOfBirds = 28;
+
+        katya.say();
+        System.out.println();
+
+
+        Human artem = new Human();
+        artem.name = "Артем";
+        artem.town = "Москва";
+        artem.yearOfBirds = 27;
+
+        artem.say();
+        System.out.println();
+
+
     }
 }
