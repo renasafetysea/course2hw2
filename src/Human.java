d86e2178cfaa5a9b24fa3b6c9e5b11c8f0649d3e
Anna Molodtsova <renasafetysea@mail.ru>
2022-09-24 19:22:43 +0300

    1,2 task / 3

--- src/Human.java
+++ src/Human.java
@@ -1,15 +1,23 @@
+import java.util.Objects;
+
 public class Human {
-    String name;
-    String town;
-    int yearOfBirds;
-    String job;
+    private String name;
+    private String town;
+    private int yearOfBirds;
+    private String job;
+
 
     public Human(String name, String town, int yearOfBirds, String job) {
-        this.name = name;
-        this.town = town;
-        this.yearOfBirds = yearOfBirds;
-        this.job = job;
+        this.name = Objects.requireNonNullElse(name,"Информация не указана!");
+        this.town = Objects.requireNonNullElse(town,"Информация не указана!");
+        if (yearOfBirds >= 0) {
+            this.yearOfBirds = yearOfBirds;
+        }else {
+            this.yearOfBirds = Math.abs(yearOfBirds);
+        }
+        this.job = Objects.requireNonNullElse(job,"Информация не указана!");
     }
+
 
     @Override
     public String toString() {
--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        Human maksim = new Human("Максим","Минск",35,"бренд-менеджером");
+        Human maksim = new Human("Максим","Минск",-20,"бренд-менеджером");
         System.out.println(maksim);
         System.out.println();
 
-        Human anya = new Human("Аня","Москва",29,"методистом образовательных программ");
+        Human anya = new Human(null,"Москва",29,"методистом образовательных программ");
         System.out.println(anya);
         System.out.println();
 
 
-        Human katya = new Human("Катя","Калининград",28,"продакт-менеджером");
+        Human katya = new Human("Катя",null,28,"продакт-менеджером");
         System.out.println(katya);
         System.out.println();
 
 
-        Human artem = new Human("Артем","Москва",27,"директором по развитию бизнеса");
+        Human artem = new Human("Артем","Москва",27,null);
         System.out.println(artem);
         System.out.println();
 
